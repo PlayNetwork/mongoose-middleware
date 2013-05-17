76934e46ffcf519c2c1b9b80e8b0c34feff7c4c7
Stanford Chiang <schiang@playnetwork.com>
2013-05-17 16:19:12 -0700

    implemented "optional search"
    Signed-off-by: Stanford Chiang <schiang@playnetwork.com>

--- filter.js
+++ filter.js
@@ -1,7 +1,7 @@
 var Query = require('mongoose').Query;
 
 
-function escapeRegex(str) {
+function sanitize(str) {
 	'use strict';
 
 	// santizes regex escapes
@@ -19,29 +19,101 @@ Query.prototype.filter = function (options) {
 	var
 		key = null,
 		mandatory = options.filters.mandatory || {},
+		optional = options.filters.optional || {},
 		self = this,
 		value = null;
 
-	for (key in mandatory.contains) {
-		if (mandatory.contains.hasOwnProperty(key)) {
-			value = mandatory.contains[key];
-			self.where(key).regex(new RegExp(escapeRegex(value), 'i'));
+	var applyWhere = function (spec, buildRegex) {
+		for (key in spec) {
+			if (spec.hasOwnProperty(key)) {
+				var val = spec[key];
+				self.where(key).regex(buildRegex(val));
+			}
 		}
-	}
+	};
 
-	for (key in mandatory.startsWith) {
-		if (mandatory.startsWith.hasOwnProperty(key)) {
-			value = mandatory.startsWith[key];
-			self.where(key).regex(new RegExp('^' + escapeRegex(value), 'i'));
-		}
-	}
+	var applyOptionals = function(optional) {
+		var items = [];
 
-	for (key in mandatory.exact) {
-		if (mandatory.exact.hasOwnProperty(key)) {
-			value = mandatory.exact[key];
-			self.where(key).regex(new RegExp('^' + escapeRegex(value) + '$', 'i'));
+		var populate = function (key, value, buildRegex) {
+			var
+				items = [],
+				item = {};
+
+			if (value instanceof Array && value.length) {
+				value.forEach(function (val) {
+					item = {};
+					item[key] = buildRegex(val);
+					items.push(item);
+				});
+				return items;
+			} else {
+				item[key] = buildRegex(value);
+				return item;
+			}
+		};
+
+		var parse = function(spec, buildRegex) {
+			var
+				parsedItems = null,
+				result = null;
+
+			if (!spec) {
+				return null;
+			}
+
+			for (key in spec) {
+				if (spec.hasOwnProperty(key)) {
+					result = populate(key, spec[key], buildRegex);
+
+					if (result instanceof Array && result.length) {
+						parsedItems = result;
+					} else {
+						if (!parsedItems) {
+							parsedItems = [];
+						}
+						parsedItems.push(result);
+					}
+				}
+			}
+			return parsedItems;
+		};
+
+		items = parse(optional.contains, regexContains);
+		if (items && items instanceof Array && items.length) {
+			self.or(items);
 		}
-	}
+
+		items = parse(optional.startsWith, regexStartsWith);
+		if (items && items instanceof Array && items.length) {
+			self.or(items);
+		}
+
+		items = parse(optional.exact, regexExact);
+		if (items && items instanceof Array && items.length) {
+			self.or(items);
+		}
+	};
+
+	var regexContains = function (val) {
+		return new RegExp(sanitize(val), 'i');
+	};
+
+	var regexStartsWith = function (val) {
+		return new RegExp('^' + sanitize(val), 'i');
+	};
+
+	var regexExact = function (val) {
+		return new RegExp('^' + sanitize(val) + '$', 'i');
+	};
+
+	// MANDATORY
+	applyWhere(mandatory.contains, regexContains);
+	applyWhere(mandatory.startsWith, regexStartsWith);
+	applyWhere(mandatory.exact, regexExact);
+
+	// OPTIONAL
+	applyOptionals(optional);
 
 	return self;
 };
