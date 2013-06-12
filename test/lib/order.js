04dd953a17f603434e091f5b913739dacd6fadf5
brozeph <joshua.thomas@gmail.com>
2013-06-11 22:57:58 -0700

    adding units for sorting

--- test/lib/order.js
+++ test/lib/order.js
@@ -1,1 +1,75 @@
-requireWithCoverage('order');
+var mongoose = require('mongoose');
+
+describe('order', function () {
+
+	var sortClauseItems = [];
+
+	before(function () {
+		requireWithCoverage('order');
+
+		mongoose.Query.prototype.sort = function (clause) {
+			if (clause) {
+				sortClauseItems.push(clause);
+			}
+		};
+	});
+
+	beforeEach(function () {
+		sortClauseItems = [];
+	});
+
+	it ('should return a query when created', function () {
+		var query = kitteh
+			.find()
+			.order(null);
+
+		(query instanceof mongoose.Query).should.equals(true);
+	});
+
+	it ('should sort fields in descending order when supplied', function () {
+		var options = {
+			sort : {
+				desc : ['name']
+			}
+		};
+
+		var query = kitteh
+			.find()
+			.order(options);
+
+		sortClauseItems.should.have.length(1);
+		sortClauseItems[0].name.should.equals(-1);
+	});
+
+	it ('should sort fields in ascending order when supplied', function () {
+		var options = {
+			sort : {
+				asc : ['name']
+			}
+		};
+
+		var query = kitteh
+			.find()
+			.order(options);
+
+		sortClauseItems.should.have.length(1);
+		sortClauseItems[0].name.should.equals(1);
+	});
+
+	it ('should sort fields in both ascending and descending order when supplied', function () {
+		var options = {
+			sort : {
+				asc : ['home'],
+				desc : ['name']
+			}
+		};
+
+		var query = kitteh
+			.find()
+			.order(options);
+
+		sortClauseItems.should.have.length(2);
+		sortClauseItems[0].name.should.equals(-1);
+		sortClauseItems[1].home.should.equals(1);
+	});
+});
