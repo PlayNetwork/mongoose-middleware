3791248844d6bf6e1b7c8191104dcbb47794d065
brozeph <joshua.thomas@gmail.com>
2013-06-13 16:09:06 -0700

    reworking component so that mongoose is not required except when testing... changes interface for component as well, but simplifies how to require it

--- test/common.js
+++ test/common.js
@@ -1,20 +1,22 @@
-var
-	mongoose = require('mongoose'),
-	Schema = mongoose.Schema;
+global.mongoose = require('mongoose'),
 
 global.requireWithCoverage = function (libName) {
 	if (process.env.MONGOOSE_MIDDLEWARE_COVERAGE) {
 		return require('../lib-cov/' + libName + '.js');
 	}
 
-	return require('../lib/' + libName + '.js');
+	if (libName === 'index') {
+		return require('../lib');
+	} else {
+		return require('../lib/' + libName + '.js');
+	}
 };
 
 global.chai = require('chai');
 global.expect = chai.expect;
 global.Kitteh = mongoose.model(
 	'kittehs',
-	new Schema({
+	new mongoose.Schema({
 		birthday : { type : Date, default : Date.now },
 		features : {
 			color : String,
